b2ce3ab4306c3edea32d91a4997d13563a7b63cc
Areskul <areskul@areskul.com>
2023-03-03 17:59:25 +0100

    good package json with auto generate d.ts

---
+++ mod.d.ts
@@ -0,0 +1,32 @@
+declare type Config = {
+    pipelines?: Pipeline[];
+};
+declare type Pipeline = {
+    name: string;
+    steps: StepOrParallel[];
+    triggers?: Trigger[];
+    on_failure?: StepOrParallel[];
+    on_success?: StepOrParallel[];
+    on_abortion?: StepOrParallel[];
+};
+declare type StepOrParallel = Step | Parallel;
+declare type Parallel = {
+    parallel: Step[];
+    on_failure?: StepOrParallel[];
+    on_success?: StepOrParallel[];
+    on_abortion?: StepOrParallel[];
+};
+declare type Step = {
+    non_blocking?: boolean;
+    name: string;
+    commands: string[];
+    on_failure?: StepOrParallel[];
+    on_success?: StepOrParallel[];
+    on_abortion?: StepOrParallel[];
+};
+declare type Trigger = {
+    branches?: string[];
+    actions?: Action[];
+};
+declare type Action = "applypatch-msg" | "pre-applypatch" | "post-apply-patch" | "pre-commit" | "prepare-commit-msg" | "commit-msg" | "post-commit" | "pre-rebase" | "post-checkout" | "post-merge" | "pre-receive" | "update" | "post-receive" | "post-update" | "pre-auto-gc" | "post-rewrite" | "pre-push" | "manual";
+export type { Config, Pipeline, StepOrParallel, Step, Parallel, Action, Trigger, };
---
+++ mod.ts
@@ -0,0 +1,60 @@
+type Config = {
+  pipelines?: Pipeline[];
+};
+type Pipeline = {
+  name: string;
+  steps: StepOrParallel[];
+  triggers?: Trigger[];
+  on_failure?: StepOrParallel[];
+  on_success?: StepOrParallel[];
+  on_abortion?: StepOrParallel[];
+};
+
+type StepOrParallel = Step | Parallel;
+type Parallel = {
+  parallel: Step[];
+  on_failure?: StepOrParallel[];
+  on_success?: StepOrParallel[];
+  on_abortion?: StepOrParallel[];
+};
+type Step = {
+  non_blocking?: boolean;
+  name: string;
+  commands: string[];
+  on_failure?: StepOrParallel[];
+  on_success?: StepOrParallel[];
+  on_abortion?: StepOrParallel[];
+};
+type Trigger = {
+  branches?: string[];
+  actions?: Action[];
+};
+type Action =
+  | "applypatch-msg"
+  | "pre-applypatch"
+  | "post-apply-patch"
+  | "pre-commit"
+  | "prepare-commit-msg"
+  | "commit-msg"
+  | "post-commit"
+  | "pre-rebase"
+  | "post-checkout"
+  | "post-merge"
+  | "pre-receive"
+  | "update"
+  | "post-receive"
+  | "post-update"
+  | "pre-auto-gc"
+  | "post-rewrite"
+  | "pre-push"
+  | "manual";
+
+export type {
+  Config,
+  Pipeline,
+  StepOrParallel,
+  Step,
+  Parallel,
+  Action,
+  Trigger,
+};
